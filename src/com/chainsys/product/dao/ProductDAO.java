43063ca1baf8e738aab99d3b93328246ef09a8bb
Roahinth D <roah2598@global.chainsys.com>
2021-05-26 10:34:47 +0530

    first commit

---
+++ src/com/chainsys/product/dao/ProductDAO.java
@@ -0,0 +1,19 @@
+package com.chainsys.product.dao;
+
+import java.util.Set;
+
+import com.chainsys.product.model.Product;
+
+public interface ProductDAO {
+	Set<Product> findAll();
+
+	Product findById(int id);
+	
+	Product findByName(String name);
+
+	void save(Product product);
+
+	void update(Product product);
+
+	void delete(int id);
+}
